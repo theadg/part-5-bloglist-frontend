d30697e385edb6fdfdef2daa83b88f1d176dbdb1
Bernard De Guzman <bernard.deguzman@technomancer.biz>
2024-02-11 11:04:40 +0800

    Add update and delete handlers

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -22,4 +22,28 @@ const store = async (blog) => {
     return response.data
 }
 
-export default { getAll, setToken, store }
+const update = async (blog) => {
+    const config = {
+        headers: {
+            Authorization: token
+        }
+    }
+
+    const response = await axios.put(`${baseUrl}/${blog.id}`, blog, config)
+
+    return response.data
+}
+
+const deleteBlog = async (blog) => {
+    const config = {
+        headers: {
+            Authorization: token
+        }
+    }
+
+    const response = await axios.put(`${baseUrl}/${blog.id}`, blog, config)
+
+    return response.data
+}
+
+export default { getAll, setToken, store, update, deleteBlog }
